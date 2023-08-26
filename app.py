a6d8d133da5f217e764f44a37eec2600ff82360b
Seyit <seyyidahmedtopbas@gmail.com>
2023-08-26 13:05:45 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ def no():
     messagebox.showinfo('BAŞARDIN', 'ADAMSIN') # messagebox ile kullanıcıya mesaj gösteriyoruz
     quit()
 
-def motionMouse(event):
+def fare(event):
     btnYes.place(x=random.randint(0, 500), y=random.randint(0, 500)) # 0 ile 500 arasında rastgele x ve y değerleri alıp butonu o konuma yerleştiriyoruz
 
 root = Tk() # tkinter penceresi oluşturuyoruz
@@ -18,6 +18,6 @@ root['bg'] = 'white' # pencerenin arkaplan rengini ayarlıyoruz
 label = Label(root, text='Gaymisin', font='Arial 20 bold', bg='white').pack() # label oluşturuyoruz
 btnYes = Button(root, text='Hayır', font='Arial 20 bold') # buton oluşturuyoruz
 btnYes.place(x=170, y=100) # butonu konumlandırıyoruz
-btnYes.bind('<Enter>', motionMouse) # butona fare ile tıklandığında motionMouse fonksiyonunu çalıştırıyoruz
+btnYes.bind('<Enter>', fare) # butona fare ile tıklandığında motionMouse fonksiyonunu çalıştırıyoruz
 btnNo = Button(root, text='Evet', font='Arial 20 bold', command=no).place(x=350, y=100) # buton oluşturuyoruz ve butona tıklandığında no fonksiyonunu çalıştırıyoruz
 root.mainloop() # pencereyi açıyoruz
